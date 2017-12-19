33f3a91d4f9d82c87a1d9a81a80a591f4b6ce334
Sara Brandes <smbrandes17@gmail.com>
2017-12-19 14:47:43 -0800

    completed through reject

--- src/underbar.js
+++ src/underbar.js
@@ -84,12 +84,26 @@
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test) {
+    var result = [];
+    _.each(collection, function(item) {
+      if (test(item)) {
+        result.push(item);
+      }
+    });
+    return result;
   };
 
   // Return all elements of an array that don't pass a truth test.
   _.reject = function(collection, test) {
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
+    var results = [];
+    _.each(collection, function (item) {
+      if (!test(item)) {
+        results.push(item);
+      }
+    })
+    return results;
   };
 
   // Produce a duplicate-free version of the array.
