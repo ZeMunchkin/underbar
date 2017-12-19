77746133709e31d51799ef47be596dea5b4321d3
Sara Brandes <smbrandes17@gmail.com>
2017-12-19 14:34:36 -0800

    Finished through IndexOf

--- src/underbar.js
+++ src/underbar.js
@@ -53,6 +53,16 @@
   // Note: _.each does not have a return value, but rather simply runs the
   // iterator function over each item in the input collection.
   _.each = function(collection, iterator) {
+    if (!Array.isArray(collection)) {
+      var objKeys = Object.keys(collection);
+      for (var i = 0; i < objKeys.length; i++) {
+        iterator(collection[objKeys[i]], objKeys[i], collection);
+      }
+    } else {
+      for (var i = 0; i < collection.length; i++) {
+        iterator(collection[i], i, collection);
+      }
+    }
   };
 
   // Returns the index at which value can be found in the array, or -1 if value
